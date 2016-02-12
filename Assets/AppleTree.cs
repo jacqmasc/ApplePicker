4d128eeef13d2aa5d696636ae25572f6cea1c848
Jackie <G00181342@W213-27.academic.raritanval.edu>
2016-02-12 12:52:43 -0500

    Added AppleTree Camera set up, prefab
    Started Basic Movement on page 431

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class AppleTree : MonoBehaviour {
 
+	// Prefab for instantiating apples
+	public GameObject applePrefab;
+
+	// Speed at which the AppleTree moves in meters/second
+	public float speed = 1f;
+
+	// Distance where AppleTree turns around
+	public float leftAndRightEdge = 10f;
+
+	// Chance that the AppleTree will change directions
+	public float chanceToChangeDirections = 0.1f;
+
+	// Rate at which Apples will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
+
 	// Use this for initialization
 	void Start () {
-	
+		// Droping apples every second
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// Basic Movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		// Changing Direction
 	}
 }
